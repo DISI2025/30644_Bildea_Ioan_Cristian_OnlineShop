db522330c8612d58d348afdfe81423c061e702bf
Nistor Ioan Gabriel <126780933+nistor1@users.noreply.github.com>
2025-05-06 20:28:12 +0300

    [Subfeature][BE][DEAL-16] - Implemented: Login functionality (#4)
    Co-authored-by: CornelPop <popc2002@yahoo.com>
    Co-authored-by: CornelPop <113294177+CornelPop@users.noreply.github.com>
    Co-authored-by: BildeaCristi <bildea.sa.ioan@student.utcluj.ro>
    Co-authored-by: AndreiZ0R <andreiborzagabriel@gmail.com>

--- backend/deal-core/src/main/java/org/deal/core/request/login/LoginRequest.java
+++ backend/deal-core/src/main/java/org/deal/core/request/login/LoginRequest.java
@@ -1,9 +1,9 @@
-package org.deal.core.request.user;
+package org.deal.core.request.login;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public record CreateUserRequest(
+public record LoginRequest(
         @JsonProperty String username, @JsonProperty String password) {
 }
