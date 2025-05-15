33984090bfdfe99f9752a675946fd48a76aa096e
Gabriel Nistor <gnistor.gabriel1473@gmail.com>
2025-05-15 10:40:30 +0300

    resolved

--- backend/product-service/src/main/resources/data.sql
+++ backend/product-service/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO "product" (id, category_name)
+INSERT INTO "product_category" (id, category_name)
 VALUES
     ('3e5b1f29-d4e8-4972-b64a-cc103086bd91', 'CAT1'),
     ('d1229c71-9e3e-4f35-9c2d-5c19fc9931a4', 'CAT2'),
