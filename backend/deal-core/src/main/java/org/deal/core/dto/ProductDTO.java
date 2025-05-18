48cca330f11e232899b295480a453cb7f27206fa
AndreiZ0R <andreiborzagabriel@gmail.com>
2025-05-18 04:22:59 +0300

    [DEAL-35] Products CRUD

---
+++ backend/deal-core/src/main/java/org/deal/core/dto/ProductDTO.java
@@ -0,0 +1,21 @@
+package org.deal.core.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+import java.util.Set;
+import java.util.UUID;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record ProductDTO(
+        UUID id,
+        String title,
+        String description,
+        Double price,
+        Integer stock,
+        String imageUrl,
+        Set<ProductCategoryDTO> categories,
+        Timestamp createdAt
+) implements Serializable {
+}
