06b07e743853cb2632f005e95e3b44f3026470d8
AndreiZ0R <andreiborzagabriel@gmail.com>
2025-05-19 22:38:29 +0300

    [Subfeature][DEAL-35][BE] Products CRUD (#31)

--- backend/product-service/src/main/java/org/deal/productservice/controller/ProductController.java
+++ backend/product-service/src/main/java/org/deal/productservice/controller/ProductController.java
@@ -8,7 +8,15 @@ import org.deal.core.request.product.UpdateProductRequest;
 import org.deal.core.response.DealResponse;
 import org.deal.productservice.service.ProductService;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.UUID;
@@ -43,7 +51,7 @@ public class ProductController {
     }
 
     @GetMapping("/seller")
-    public DealResponse<List<ProductDTO>> getProductsBySellerId(@RequestParam final UUID id){
+    public DealResponse<List<ProductDTO>> getProductsBySellerId(@RequestParam final UUID id) {
         return productService.findAllBySellerId(id)
                 .map(DealResponse::successResponse)
                 .orElse(DealResponse.failureResponse(
